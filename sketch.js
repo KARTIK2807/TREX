0dbe5fc4f2ff586302c350aa9b2ec1600a8c8b24
KARTIK AGARWAL <91669048+KARTIK2807@users.noreply.github.com>
2021-11-08 20:38:33 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,25 +1,40 @@
 
 function setup() {
   createCanvas(400,400);
-  var box = createSprite(200,200,30,30);
+  background(51);
+  box = createSprite(200,200,30,30);
+
 }
 
 function draw() 
 {
-  background("red");
+
+  if (keyIsDown(RIGHT_ARROW))
+  {
+    background("red");
+  }
   
-  if(keyIsDown(RIGHT_ARROW)){
-    box.x=box.x+2;
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
   }
-  if(keyIsDown(LEFT_ARROW)){
-    box.x=box.x-2;
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
   }
-  if(keyIsDown(UP_ARROW)){
-    box.y=box.y-2;
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
   }
-  if(keyIsDown(DOWN_ARROW)){
-    box.y=box.y+2;
-  }
+
+
+  
   drawSprites();
 }
 
